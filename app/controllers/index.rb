2679ad907875acfab17f5f22ba51a0bd600e03af
herbedev <herbener.dev@gmail.com>
2017-09-06 14:42:37 -0400

    Solo-john

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,19 +10,34 @@ end
 post '/posts/:id/vote' do
   post = Post.find(params[:id])
   post.votes.create(value: 1)
-  redirect "/posts"
+  if request.xhr?
+    post.votes.count.to_s
+  else
+    redirect '/posts'
+  end
 end
 
 delete '/posts/:id' do
   # write logic for deleting posts here.
+  post = Post.find(params[:id])
+  post.destroy
 end
 
+
 post '/posts' do
-  Post.create( title: params[:title],
-               username: Faker::Internet.user_name,
-               comment_count: rand(1000) )
-  redirect '/posts'
+  @post = Post.new(title: params[:title],username: Faker::Internet.user_name,comment_count: rand(1000))
+  if @post.save
+    status 200
+    if request.xhr?
+       erb :'partials/_article', locals: {post: @post}, layout: false
+    else
+      redirect '/posts'
+    end
+  else
+    status 422
+  end
 end
+
 
 get '/post/:id' do
   @post = Post.find(params[:id])
--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,49 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  $( ".post-container" ).on("click", "button.vote-button", function(e) {
+    e.preventDefault();
+
+    var type = $(this).parent().attr("method");
+    var url = $(this).parent().attr("action");
+    var this_placeholder = $(this);
+
+    $.ajax({
+     type: type,
+     url: url,
+   })
+   .done(function(response) {
+     $(this_placeholder).parent().siblings("p").children(".points").text(response);
+     $(this_placeholder).css("color","red");
+   })
+  });
+
+  $(".post-container").on("click", "form a.delete", function(e) {
+    e.preventDefault();
+    var type = "delete";
+    var url = $(this).parent().attr("action");
+    $.ajax({
+     type: type,
+     url: url,
+    })
+    $(this).parents("article").remove();
+  });
+
+  $( "#posts" ).submit(function(e) {
+    e.preventDefault();
+
+    var type = $(this).attr("method");
+    var url = $(this).attr("action");
+    var data = $(this).serialize();
+
+    $.ajax({
+     type: type,
+     url: url,
+     data: data
+   })
+   .done(function(response) {
+      $(".post-container").append(response);
+   })
+  });
 });
