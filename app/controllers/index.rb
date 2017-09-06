4c58a3ebe29d6e36394e17710ac14a3d8a5a040c
Abraham Kwak <kwak.abe@gmail.com>
2017-09-06 16:37:30 -0400

    create partials and delete function.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,19 +8,29 @@ get '/posts' do
 end
 
 post '/posts/:id/vote' do
-  post = Post.find(params[:id])
-  post.votes.create(value: 1)
-  redirect "/posts"
+  @post = Post.find(params[:id])
+  @post.votes.create(value: 1)
+  if request.xhr?
+    # post.points.to_s
+    content_type :json
+    { points: @post.points.to_s }.to_json
+  else
+    redirect "/posts"
+  end
 end
 
 delete '/posts/:id' do
-  # write logic for deleting posts here.
+  @post = Post.find_by(id: params[:id])
+  if request.xhr?
+    @post.destroy
+  else
+    @post.destroy
+    redirect "/posts"
+  end
 end
 
 post '/posts' do
-  Post.create( title: params[:title],
-               username: Faker::Internet.user_name,
-               comment_count: rand(1000) )
+  Post.create( title: params[:title], username: Faker::Internet.user_name, comment_count: rand(1000) )
   redirect '/posts'
 end
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,34 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $(".post-container").on("submit", ".inline", function(event) {
+    event.preventDefault();
+    let $upvoteForm = $(this);
+    let $upvoteButton = $upvoteForm.find(".upvote-button");
+    let url = $upvoteForm.attr("action");
+    let type = $upvoteForm.attr("method");
+    let $points = $upvoteForm.closest("article").find(".points");
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    $.ajax({
+      url: url,
+      type: type
+    })
+    .done(function(response) {
+      $points.replaceWith(response.points);
+      $upvoteButton.css("color", "lightgreen");
+    })
+  });
+
+  $(".post-container").on("submit", ".delete", function(event){
+      event.preventDefault();
+      let $deleteForm = $(this)
+      let $article = $deleteForm.closest("article");
+      let url = $deleteForm.attr("action");
+
+      $.ajax({
+        url: url,
+        type: "DELETE"
+      })
+      .done(function(response){
+        $article.remove();
+      });
+    });
 });
