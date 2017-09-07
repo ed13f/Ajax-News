9b18f745783f19e8ab7ff7a74aa14c25878b6777
RicoDe Santis <ricodesantis@gmail.com>
2017-09-07 10:38:43 -0400

    Complete assignment

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,18 +10,33 @@ end
 post '/posts/:id/vote' do
   post = Post.find(params[:id])
   post.votes.create(value: 1)
-  redirect "/posts"
+  if request.xhr?
+    post.votes.length.to_s
+  else
+    redirect "/posts"
+  end
 end
 
 delete '/posts/:id' do
-  # write logic for deleting posts here.
+  if request.xhr?
+    Post.find_by(id: params[:id]).destroy
+  else
+    Post.find_by(id: params[:id]).destroy
+    redirect '/posts'
+  end
 end
 
 post '/posts' do
-  Post.create( title: params[:title],
+    post = Post.new( title: params[:title],
                username: Faker::Internet.user_name,
                comment_count: rand(1000) )
-  redirect '/posts'
+  if request.xhr? && post.save
+    status 200
+    erb :"_article", layout: false, locals: { post: post }
+  else
+    status 422
+    redirect '/posts'
+  end
 end
 
 get '/post/:id' do
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,49 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $(".upvote_form").on("submit", function(event){
+    event.preventDefault();
+    var $form = $(this)
+    var $article = $(this).closest("article").attr('id');
+    $.ajax({
+      url: "/posts/"+$article+"/vote",
+      method: $form.attr('method')
+    })
+    .done(function(response){
+      $form.find(".upvote-button").css("color", "green");
+      $("article#"+$article).children().children(".points").text(response);
+    })
+  })
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $(".delete").click(function(event){
+    event.preventDefault()
+    var $articleSection = $(this).closest('article')
+    var $article = $(this).parent().parent().attr('id');
+    $.ajax({
+      url: '/posts/'+ $article,
+      method: "delete"
+    })
+    .done(function(response) {
+      $articleSection.remove();
+    })
+  })
+
+  $("#posts").submit(function(event){
+    event.preventDefault()
+    var $form = $(this);
+    var $tableData = $form.serialize()
+    var $container = $form.siblings(".post_container")
+    $.ajax({
+      url: '/posts',
+      method: "post",
+      data: $tableData
+    })
+    .done(function(response){
+      $(".post-container").append(response)
+      $("#text_feild").val("")
+    })
+  })
+  $(".post-container").delegate(".delete", "click", function(){
+    $(this).closest("article")
+  })
 });
+
+
